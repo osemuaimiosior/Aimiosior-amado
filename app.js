08a3a650462f8db8f9e519609db2b3d88a880c4d
Osemu <vincentaimiosior@gmail.com>
2024-01-07 23:18:52 +0100

    updates

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
 const logEvents = require('./logEvents');
 const EventEmitter = require('events');
 
-class MyEmitter extends EventEmitter { };
+const PORT = process.env.PORT || 3500;
 
-const myEmitter = new MyEmitter();
+class Emitter extends EventEmitter {};
 
+const myEmitter = new Emitter();
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+/*
 myEmitter.on('log', (msg) => logEvents(msg));
 
-setTimeout(() => {
     myEmitter.emit('log', 'Log event emitted!');
-}, 2000);
+    */
