bae35e2984869755533abfaf0b57fd77626f95ae
Osemu <vincentaimiosior@gmail.com>
2024-01-09 23:28:07 +0100

    updates

--- app.js
+++ app.js
@@ -4,6 +4,30 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
+// function one
+/*const actionOne = (req, res) => {
+    res.sendFile(path.join(__dirname, 'AboutUs.html'));
+    next();
+};
+
+// function two
+const actionTwo = (req, res) => {
+    res.sendFile(path.join(__dirname, 'AboutUs.html'));
+    next();
+};
+
+// If link is pressed, run function actionOne and actionTwo
+app.get('^/$|/index(.html)?', [actionOne, actionTwo]);*/
+
+// To handle form data
+app.use(express.urlencoded({ extended: false}));
+
+// built-in middleware for json
+app.use(express.json());
+
+//Serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
