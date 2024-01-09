f7b33c5a15f77edec013e556b79cecbd05ac2f30
Osemu <vincentaimiosior@gmail.com>
2024-01-09 01:00:58 +0100

    updates

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/teleComm.html(.html)?', (req, res) => {
+app.get('../index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','index.html'));
+});
+
+app.get('/teleComm(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'teleComm.html'));
+});
+
+app.get('/realEstateHome(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'realEstateHome.html'));
+});
+
+app.get('/AboutUs(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'AboutUs.html'));
 });
 
 app.get('/Dashboard/dashBoardIndex(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'Dashboard', 'dashBoardIndex.html'));
+});
+
+app.get('/product-listing/product-details-2015-Benz(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'product-listing','product-details-2015-Benz.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
