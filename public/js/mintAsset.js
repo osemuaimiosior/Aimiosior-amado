c1f3c96da4b2ce2e79e701d6ec506f68b304e618
Osemu <vincentaimiosior@gmail.com>
2024-01-13 12:39:33 +0100

    updates

--- public/js/mintAsset.js
+++ public/js/mintAsset.js
@@ -1,5 +1,17 @@
 
-function mintAsset(){
+const mAsset = () => {
     window.open("../mintAsset.html", "", "width=500,height=550");
 }
+
+const mintAsset = async () => {
+    const accounts = await window.ethereum.request({method: "eth_accounts"})
+    if (accounts.length){
+            mAsset();
+        // console.log("wallect connected");
+    } else{
+        setTimeout(function(){alert ("Please connect your wallet")}, 1000);
+    }
+}
+
+module.exports = mintAsset;
      
--- routes/productListing.js
+++ routes/productListing.js
@@ -10,4 +10,16 @@ router.get('/product-details-2016-TeslaModelS(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..','product-listing','product-details-2016-TeslaModelS.html'));
 });
 
+router.get('/product-details-2015-BMW335(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','product-listing','product-details-2015-BMW335.html'));
+});
+
+router.get('/product-details-2015-Toyota-Rav4(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','product-listing','product-details-2015-Toyota-Rav4.html'));
+});
+
+router.get('/product-details-2016-NissanMurano-Hybrid(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','product-listing','product-details-2016-NissanMurano-Hybrid.html'));
+});
+
 module.exports = router;
