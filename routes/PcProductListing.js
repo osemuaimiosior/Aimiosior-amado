ac1f330863eea1693302a6644c67f5b07650096d
Osemu <vincentaimiosior@gmail.com>
2024-02-03 20:23:47 +0100

    updates

--- routes/PcProductListing.js
+++ routes/PcProductListing.js
@@ -18,4 +18,32 @@ router.get('/product-details-LenovoAMDRYZEN(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..','product-listing','PC','product-details-LenovoAMDRYZEN.html'));
 });
 
+router.get('/product-details-EliteBook840(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','product-listing','PC','product-details-EliteBook840.html'));
+});
+
+router.get('/product-details-macbookPro(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','product-listing','PC','product-details-macbookPro.html'));
+});
+
+router.get('/product-details-DELLCHROMEBOOK11(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','product-listing','PC','product-details-DELLCHROMEBOOK11.html'));
+});
+
+router.get('/product-details-HpPavilion15(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','product-listing','PC','product-details-HpPavilion15.html'));
+});
+
+router.get('/product-details-Hp14(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','product-listing','PC','product-details-Hp14.html'));
+});
+
+router.get('/product-details-DellVostro(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..','product-listing','PC','product-details-DellVostro.html'));
+});
+
 module.exports = router;
+
+
+
+
