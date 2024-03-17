70265c71e6f4828e29a2a878f803ba615f7c4bcb
Osemu <vincentaimiosior@gmail.com>
2024-03-17 14:31:58 +0000

    updates

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('events').EventEmitter.defaultMaxListeners = 0
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
